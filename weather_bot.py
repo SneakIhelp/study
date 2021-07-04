cb60851ee189e6e17d332c8ab74546a6025c8b5a
SneakIhelp <50763685+SneakIhelp@users.noreply.github.com>
2021-07-04 18:49:50 +0300

    Update weather_bot.py

--- weather_bot.py
+++ weather_bot.py
@@ -1,8 +1,13 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from pyowm.owm import OWM
+from pyowm.utils.config import get_default_config
 
-owm = OWM('5e53f266fcc1e46be38bb6d8a9a0ab61')
-place = 'Moscow'
+
+config_dict = get_default_config()
+config_dict['language'] = 'ru'
+
+owm = OWM('5e53f266fcc1e46be38bb6d8a9a0ab61', config_dict)
+place = 'Москва'
 mgr = owm.weather_manager()
 observation = mgr.weather_at_place(place)
 w = observation.weather
@@ -13,21 +18,20 @@ t2 = temp['temp_max']
 t3 = temp['temp_min']
 t4 = (t3 + t2) / 2
 
-genius = 'В городе ' + str(place) + ' температура ' + str(round(t)) + ' °C, ощущается как ' + str(round(t1)) + '°C, ' \
-         'максимальная тепература ' + str(
-    round(t2)) + \
-         ' °C, минимальная температура ' + str(round(t3)) + ' °C, средняя тепература за день ' + \
-         str(round(t4)) + ' °C '
+genius = 'Сейчас в городе ' + str(place) + ' температура ' + str(t) + ' °C, ощущается как ' + str(t1) + '°C, ' \
+         'максимальная тепература ' + str(t2) + \
+         ' °C, минимальная температура ' + str(t3) + ' °C, средняя тепература в этот день ' + \
+         str(t4) + ' °C '
 try:
     monitoring = owm.weather_manager().weather_at_place(place)
     weather = monitoring.weather
     status = weather.detailed_status
-    helpme = 'It is ' + str(status) + ' in ' + str(place) + ' now. '
+    helpme = 'Сейчас ' + 'в городе ' + str(place) + ' ' + str(status) + '.'
 except:
     pass
 
 
-print("Бот запущен. ")
+print("Бот запущен. Ctrl + C для закрытия")
 
 
 def on_start(update, context):
