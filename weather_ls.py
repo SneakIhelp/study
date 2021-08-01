95684f26a5dae83c2d58fd20ca45e0e961366db5
SneakIhelp <50763685+SneakIhelp@users.noreply.github.com>
2021-08-01 16:35:26 +0300

    Update weather_ls.py

--- weather_ls.py
+++ weather_ls.py
@@ -1,6 +1,11 @@
 import telethon
 from pyowm.owm import OWM
+from pyowm.utils.config import get_default_config
 import asyncio
+
+
+config_dict = get_default_config()
+config_dict['language'] = 'ru'
 
 owm = OWM('5e53f266fcc1e46be38bb6d8a9a0ab61')
 
@@ -15,18 +20,15 @@ t2 = temp['temp_max']
 t3 = temp['temp_min']
 t4 = (t3 + t2) / 2
 
-genius = 'В городе ' + str(place) + ' температура ' + str(round(t)) + ' °C, ощущается как ' + str(round(t1)) + '°C, ' \
-         'максимальная тепература ' + str(
-    round(t2)) + \
-         ' °C, минимальная температура ' + str(round(t3)) + ' °C, средняя тепература за день ' + \
-         str(round(t4)) + ' °C '
-try:
-    monitoring = owm.weather_manager().weather_at_place(place)
-    weather = monitoring.weather
-    status = weather.detailed_status
-    helpme = 'It is ' + str(status) + ' in ' + str(place) + ' now. '
-except:
-    pass
+genius = f'Сейчас в городе {str(place)} температура {str(t)} °C, ощущается как {str(t1)} °C, ' \
+         f'максимальная температура {str(t2)} °C, минимальная температура {str(t3)} °C, средняя температура ' \
+         f'сегодня {str(t4)} °C '
+
+monitoring = owm.weather_manager().weather_at_place(place)
+weather = monitoring.weather
+status = weather.detailed_status
+helpme = f'Сейчас в городе {str(place)} {str(status)}.'
+
 
 api_id = 6956069
 api_hash = 'e846851c160ab65697bb39404f40ae1c'
